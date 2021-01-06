80d5c5d115a6f0316c6236bd6075343a38753ddf
marek <marek.anderson@arctiq.ca>
2021-01-06 21:09:58 +0000

    initial

---
+++ main.tf
@@ -0,0 +1,9 @@
+module "gke1" {
+  source  = "tfe.manderson.2741611427.workshops.arctiq.ca/arctiq/gke/google"
+  version = "1.0.1"
+ 
+  # insert required variables here
+  cluster_name = var.cluster_name
+  project_id=var.gcp_project_id
+
+}
